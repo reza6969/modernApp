4d8c7f2a0054e52cf32868d4a4d823924b5ee8bf
r.soltani <infooffice1399@gmail.com>
2024-01-26 22:12:04 +0330

    Adding the Movie Database API & Creating a Service &  build a view around our isLoading state and the value from our API call:  If we are loading, we show a list of six Ionic skeleton items If we have an error, we show an Ionic inline alert with the error message When we have an array, we show a list of movies When the array is empty and we finished loading, we show a message that no movies were found.
    :anchor: Simply create an account and request one for free, then open the src/environments/environment.ts file and add your API key
    :-1:  this results in some interfaces we can rename and put into a new
    :100: we can create a new service to fetch the movie data from the API. Services haven’t changed much but they don’t have to: React developers would love to have a singleton service construct to inject anywhere!
    :alien: Services structure our Angular app, split up view and business logic and make it easy to share data between components.
    :airplane: our service to make two API calls inside the src/app/services/movie.service.ts
    :-1: next step now also moved the dependency injection to the class level by using inject directly.
    :apple: Using the new Angular Control Flow
    :airplane: building our app by adding a new page to show the list of trending movies, based on the data we get from our MovieService. Additionally
    :+1: we will now add imports for all Ionic components that we will later use in our view.
    :+1: Instead of importing all Ionic components, we can now pick the ones we actually need from the @ionic/angular/standalone package and include them in our Angular standalone components.
    :+1: While this does add a few lines to our imports, it also makes it easier to see which components we actually use in our view. Additionally, this makes loading pages faster and reduces the final bundle size of our app.
    :+1: We now need one function to load the trending movies, and another one to load more movies when we scroll to the bottom of the list which triggers the infinite scroll event.
    :apple: The new Angular Control Flow is a powerful feature that allows us to write cleaner and more readable code.
    :100: It’s based on the @if and @for directives, which are similar to the *ngIf and *ngFor directives, but with some important differences.
    :accept: The @if directive is used to conditionally render a block of code, and it can be used just as you would expect – but inside the HTML
    :100: The second new directive is @for, which is used to iterate over an array and render a block of code for each item in the array:
    :+1:  build a view around our isLoading state and the value from our API call:
    
    If we are loading, we show a list of six Ionic skeleton items
    If we have an error, we show an Ionic inline alert with the error message
    When we have an array, we show a list of movies
    When the array is empty and we finished loading, we show a message that no movies were found.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiKey: '2b84ada90179ff4b26f6ad7d947e7992',
 };
 
 /*
